41ef9601e774de3f974afe6d1301e9281c862a56
Nat1anWasTaken <nathan@nat1an.xyz>
2025-05-03 14:21:41 +0800

    feat(lib/mongodb.ts): add MongoDB client setup and configuration

---
+++ lib/mongodb.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = { appName: "devrel.template.nextjs" };
+
+let client: MongoClient;
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  client = globalWithMongo._mongoClient;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+}
+
+export default client;
+export const database = client.db(process.env.DB_NAME ? process.env.DB_NAME : "cert-vote");
